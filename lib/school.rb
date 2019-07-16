580f49465c29b10f0ac237835f4a63d820de7007
Alyssa Farina <alyssafarina695@gmail.com>
2019-07-16 14:03:43 -0400

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,21 @@
-# code here!
+class School
+    def initialize(model)
+        @model = model
+    end
+
+    school.roster = {
+        :grade_levels = [ 
+
+        ]
+    }
+
+    school.add_student("Zach Morris", 9)
+    school.add_student("Kelly Kapowski", 10)
+    school.add_student("Screech", 11)
+    school.roster
+
+    school.grade_levels(9)
+
+    school.sort
+end
+end
