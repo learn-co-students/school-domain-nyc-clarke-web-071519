c78510ada9a3646852f9d908d2710b263d3454c3
Valentino Wong <valentino.wong@gmail.com>
2019-07-16 14:17:11 -0400

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,36 @@
 # code here!
+require 'pry'
+
+class School
+    
+    def initialize(name)
+        @name = name
+        @roster = {}
+    end
+
+    def roster
+        @roster
+    end
+
+    def add_student(student_name, student_grade)
+        if roster[student_grade]
+            roster[student_grade] << student_name
+        else
+            roster[student_grade] = []
+            roster[student_grade] << student_name
+        end
+    end
+
+    def grade(student_grade)
+        roster[student_grade]
+    end
+
+    def sort
+        new_roster = {}
+        roster.map do |student_grade,student_names|
+           new_roster[student_grade] = student_names.sort 
+        end
+        new_roster
+    end
+
+end
